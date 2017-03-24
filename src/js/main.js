ff4960bd7ea31438d5138adf0d8e616986c3a98a
Diego Rayo <diegofrayo@gmail.com>
2017-03-23 19:49:46 -0500

    [create] project structure and initial files

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+var BookmarksUtil = {
+	OTHER_BOOKMARKS_FOLDER_ID: '2',
+	RSS_CHANNELS_FOLDER_NAME: 'RSS Reader - Channels',
+	createBookmark: function() {
+
+	},
+	deleteBookmark: function() {
+
+	},
+	getFolder: function(folderId) {
+		chrome.bookmarks.getChildren(folderId, function(results) {});
+	}
+};
+
+function getXMLFile(url) {
+
+	$.ajax(url, {
+		accepts: {
+			xml: 'application/rss+xml'
+		},
+		dataType: 'xml',
+		success: function(data) {
+			$(data).find('item').each(function() {
+				var item = $(this);
+				item.find('title').text();
+				item.find('link').text();
+				item.find('description').text();
+			});
+		},
+		error: function(data) {
+			console.log(data);
+		}
+	});
+
+}
+
+$(document).ready(function() {
+	BookmarksUtil.getFolder(BookmarksUtil.OTHER_BOOKMARKS_FOLDER_ID);
+});
